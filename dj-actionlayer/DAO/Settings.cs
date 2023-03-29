c96d9e12939912738af337811f2e2c8a4dd45537
dj-xuyenchi <do.quanganh99zz@gmail.com>
2023-03-29 08:53:33 +0700

    ok

--- dj-actionlayer/DAO/Settings.cs
+++ dj-actionlayer/DAO/Settings.cs
@@ -16,9 +16,13 @@ namespace dj_actionlayer.DAO
         {
             return "Data Source=localhost;Initial Catalog=dj_coding;Integrated Security=True;encrypt=true;trustservercertificate=true;MultipleActiveResultSets=True;";
         }
-        public static string DBConnectHosting()
+        public static string DBConnectDeploy()
         {
             return "Server=localhost;Integrated Security=false;Initial Catalog=djxuyen1_coding;MultipleActiveResultSets=True;User ID= djxuyen1_djxuyen1;Password =Mamama99@";
+        }
+        public static string DBConnectHostingRemote()
+        {
+            return "Server=125.212.224.205;Integrated Security=false;Initial Catalog=djxuyen1_coding;MultipleActiveResultSets=True;User ID= djxuyen1_djxuyen1;Password =Mamama99@";
         }
     }
 }
