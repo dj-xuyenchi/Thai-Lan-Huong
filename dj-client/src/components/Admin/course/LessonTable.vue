e7db2726c4ff2265513dbc54c05ee8cfd3fe1ae0
dj-xuyenchi <do.quanganh99zz@gmail.com>
2023-04-19 13:17:03 +0700

    up

---
+++ dj-client/src/components/Admin/course/LessonTable.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-table>
+      <thead>
+        <tr>
+          <th class="text-left">STT</th>
+          <th class="text-left">Tên bài học</th>
+          <th class="text-left">Mô tả</th>
+          <th class="text-left">Ngày tạo</th>
+          <th class="text-left">Cập nhật</th>
+          <th class="text-left">Loại bài học</th>
+          <th class="text-left">Thời lượng</th>
+          <th class="text-left">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in data" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ item.lessonName }}</td>
+          <td>{{ item.lessonDescription }}</td>
+          <td>{{ item.createDateTime }}</td>
+          <td>{{ item.updateDateTime }}</td>
+          <td>{{ item.lessonType }}</td>
+          <td>{{ item.videoTime }}</td>
+          <td>{{ item.videoTime }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LessonTable",
+  props: {
+    data: [],
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
