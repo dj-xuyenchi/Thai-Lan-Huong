0c30d3a06c8ff265f68d0ff87f5742543fd1fd8c
Quang Anh <89133721+dj-xuyenchi@users.noreply.github.com>
2023-04-20 00:48:40 +0700

    update

---
+++ dj-webdesigncore/Request/Lesson/PracticeLesson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dj_webdesigncore.Request.Lesson
+{
+    public class PracticeLesson
+    {
+      public  lessonName: "",
+    lessonDescription: "",
+    lessonTime: "",
+    problem: "",
+    problemDetail: "",
+    beginCode: "",
+    inputExemple: "",
+    outputExemple: "",
+    explainCode: "",
+    suggest: "",
+    }
+}
