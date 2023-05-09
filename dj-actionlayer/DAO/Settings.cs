4ceefddc141cf47e6ab65fecb5d08af33c34111f
dj-xuyenchi <do.quanganh99zz@gmail.com>
2023-05-09 14:18:23 +0700

    update

--- dj-actionlayer/DAO/Settings.cs
+++ dj-actionlayer/DAO/Settings.cs
@@ -10,23 +10,18 @@ namespace dj_actionlayer.DAO
     {
         public static string SecretKey()
         {
-            return "npqhdyzryqtirqiqazozklrfxdogjbdy";
         }
         public static string DBConnect()
         {
-            return "Data Source=localhost;Initial Catalog=dj_coding;Integrated Security=True;encrypt=true;trustservercertificate=true;MultipleActiveResultSets=True;";
         }
         public static string DBConnectDeploy()
         {
-            return "Server=localhost;Integrated Security=false;Initial Catalog=djxuyen1_coding;MultipleActiveResultSets=True;User ID= djxuyen1_djxuyen1;Password =Mamama99@";
         }
         public static string DBConnectHostingRemote()
         {
-            return "Server=125.212.224.205;Integrated Security=false;Initial Catalog=djxuyen1_coding;MultipleActiveResultSets=True;User ID= djxuyen1_djxuyen1;Password =Mamama99@";
         }
         public static string[] emailConfig()
         {
-            return new string[] { "do.quanganh99zz@gmail.com", "cugzmhlctsbfssga", "smtp.gmail.com", "587" };
         }
         public static string enviroment()
         {
