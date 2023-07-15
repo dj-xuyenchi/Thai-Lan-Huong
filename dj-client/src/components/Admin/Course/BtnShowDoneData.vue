c5e19e603f193c42b2ba26c65adb978730aa57cb
dj-xuyenchi <do.quanganh99zz@gmail.com>
2023-07-15 20:22:51 +0700

    update

--- dj-client/src/components/Admin/Course/BtnShowDoneData.vue
+++ dj-client/src/components/Admin/Course/BtnShowDoneData.vue
@@ -12,26 +12,68 @@
           </v-btn>
         </template>
         <v-card>
-          <v-card-title class="text-h5"> Bài làm của học viên </v-card-title>
-          <v-card-text
-            >Sếp có chắc muốn xóa học phần này sau khi xóa sẽ không thể khôi
-            phục lại.</v-card-text
-          >
+          <v-card-title>
+            <span class="text-h5">Cập nhật người dùng</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-form
+                  @submit.prevent="submit()"
+                  ref="form"
+                  enctype="multipart/form-data"
+                >
+                  <v-row
+                    ><v-col cols="12" sm="12" md="12">
+                      <img
+                        :src="data"
+                        alt="Hình ảnh"
+                        style="height: 100px; width: 100px; border-radius: 50%"
+                      />
+                      <v-label style="width: 100%; margin: 24px 0 8px 0"
+                        >Ảnh đại diện người dùng</v-label
+                      >
+                      <v-btn
+                        color="#4d96ff"
+                        style="
+                          height: 35px;
+                          width: 104px;
+                          font-size: 14px;
+                          font-weight: 400;
+                        "
+                        @click="$refs.fileInput.click()"
+                      >
+                        Chọn ảnh</v-btn
+                      >
+                      <v-file-input
+                        ref="fileInput"
+                        v-model="selectFile"
+                        accept="image/*"
+                        @change="onFileSelect"
+                        outlined
+                        dense
+                        hide-details
+                        class="my-file-input"
+                      ></v-file-input>
+                    </v-col>
+                    <v-col cols="4" sm="4" md="4">
+                      <v-text-field
+                        v-model="firstName"
+                        density="compact"
+                        label="Họ*"
+                        hint="Họ người dùng"
+                        variant="outlined"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-form>
+              </v-row>
+            </v-container>
+          </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn
-              color="green-darken-1"
-              variant="text"
-              @click="dialog = false"
-            >
-              Hủy
-            </v-btn>
-            <v-btn
-              color="green-darken-1"
-              variant="text"
-              @click="deleteCourseChapter()"
-            >
-              Đồng ý
+            <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
+              Tuyệt cú mèo
             </v-btn>
           </v-card-actions>
         </v-card>
